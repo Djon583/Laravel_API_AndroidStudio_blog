69d7cf5a34eb9da6b9b5685b9e50b584611d2e2c
king_man <63049656+Djon583@users.noreply.github.com>
2020-09-10 12:44:33 +0600

    My Second Commit

---
+++ blog/app/Http/Controllers/Api/PostsController.php
@@ -0,0 +1,93 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use App\Post;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Storage;
+
+class PostsController extends Controller
+{
+    public function create(Request $request){
+
+        $post = new Post;
+        $post->user_id = Auth::user()->id;
+        $post->desc = $request->desc;
+
+        if($request->photo != ''){
+            $photo = time().'jpg';
+            file_put_contents('storage/posts/'.$photo,base64_decode($request->photo));
+            $post->photo = $photo;
+        }
+
+        $post->save();
+        $post->user;
+        return response()->json([
+            'success' => true,
+            'message' => 'posted',
+            'post' => $post
+        ]);
+    }
+
+    public function update(Request $request){
+        $post = Post::find($request->id);
+        if(Auth::user()->id != $request->id){
+            return response()->json([
+                'success' => false,
+                'message' => 'unauthorized access'
+            ]);
+        }
+        $post->desc = $request->desc;
+        $post->update();
+        return response()->json([
+            'success' => true,
+            'message' => 'post edited'
+        ]);
+    }
+
+    public function delete(Request $request){
+        $post = Post::find($request->id);
+        if(Auth::user()->id != $request->id){
+            return response()->json([
+                'success' => false,
+                'message' => 'unauthorized access'
+            ]);
+        }
+
+        if($post->photo != ''){
+            Storage::delete('public/posts/'.$post->photo);
+        }
+        $post->delete();
+        return response()->json([
+            'success' => true,
+            'message' => 'post deleted'
+        ]);
+    }
+
+    public function posts(){
+        $posts = Post::orderBy('id','desc')->get();
+        foreach($posts as $post){
+            //get User post
+            $post->user;
+            //comments count
+            $post['commentCount'] = count($post->comments);
+            //likes count
+            $post['likesCount'] = count($post->likes);
+
+            $post['selfLike'] = false;
+            foreach($post->likes as $like){
+                if($like->user_id == Auth::user()->id){
+                    $post['selfLike'] = false;        
+                }
+            }
+        }
+
+        return response()->json([
+            'success' => true,
+            'posts' => $posts
+        ]);
+    }
+}
+
